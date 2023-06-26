69f6877cc3d48288801bc760d0e6d67aa2f0c48e
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-06-26 13:37:27 -0300

    Correção de um trecho do código do componente Galeria.

--- src/componentes/Galeria/Cards/Card/index.jsx
+++ src/componentes/Galeria/Cards/Card/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import open from './open.png';
-import favorito from './favorito.png';
+import React from "react";
+import open from "./open.png";
+import favorito from "./favorito.png";
 
-export default function Card({ foto, styles }) {
+export default function Card({ fotos, styles }) {
   return (
-    <li key={foto.id} className={styles.galeria__card}>
+    <li key={fotos.id} className={styles.galeria__card}>
       <img
         className={styles.galeria__imagem}
-        src={foto.imagem}
-        alt={foto.titulo}
+        src={fotos.imagem}
+        alt={fotos.titulo}
       />
-      <p className={styles.galeria__descricao}>{foto.titulo}</p>
+      <p className={styles.galeria__descricao}>{fotos.titulo}</p>
       <div>
-        <p>{foto.creditos}</p>
+        <p>{fotos.creditos}</p>
         <span>
-          <img src={favorito} alt='ícone de coração ou favoritar'></img>
-          <img src={open} alt='ícone de expandir modal'></img>
+          <img src={favorito} alt="ícone de coração ou favoritar"></img>
+          <img src={open} alt="ícone de expandir modal"></img>
         </span>
       </div>
     </li>
--- src/componentes/Galeria/Cards/index.jsx
+++ src/componentes/Galeria/Cards/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
 export default function Cards({ fotos, styles }) {
   return (
     <ul className={styles.galeria__cards}>
       {fotos.map((foto) => {
-        return <Card key={fotos.id} fotos={fotos} styles={styles} />
+        return <Card key={foto.id} fotos={foto} styles={styles} />;
       })}
     </ul>
-  )
+  );
 }
