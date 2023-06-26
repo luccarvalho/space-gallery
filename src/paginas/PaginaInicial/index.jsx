196920a0288f81141ed5cbbb606545b5617d6587
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-06-26 16:09:54 -0300

    Adicionando o componente Populares a página inicial.

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,27 +1,28 @@
-import Banner from '../../componentes/Banner';
-import Cabecalho from '../../componentes/Cabecalho';
-import Galeria from '../../componentes/Galeria';
-import Menu from '../../componentes/Menu';
-import Rodape from '../../componentes/Rodape';
-import styles from './PaginaInicial.module.scss';
+import Banner from "../../componentes/Banner";
+import Cabecalho from "../../componentes/Cabecalho";
+import Galeria from "../../componentes/Galeria";
+import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
+import Rodape from "../../componentes/Rodape";
+import styles from "./PaginaInicial.module.scss";
 
 export default function PaginaInicial() {
-    return(
-        <>
-            <Cabecalho/>
-            <main>
-                <section>
-                    <div className={styles.principal}>
-                        <Menu/>
-                        <Banner/>
-                    </div>
-                </section>
-            </main>
-            <div className={styles.galeria}>
-                <Galeria/>
-            </div>
-            <Rodape/>
-        </>
-    )
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section>
+          <div className={styles.principal}>
+            <Menu />
+            <Banner />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+      <Rodape />
+    </>
+  );
 }
-                    
