a9c2fad8e543db7a92649df61b58129001e84d7c
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-09-12 17:40:48 -0300

    Início da construção da nova versão do Space Gallery.

---
+++ space-gallery v2/src/App.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  width: 100%;
+  min-height: 100vh;
+`;
+
+function App() {
+  return (
+    <FundoGradiente>
+      <GlobalStyles />
+    </FundoGradiente>
+  );
+}
+
+export default App;
