eaae11cbd6810e6cbb05a2e2ed4f54c840de20b4
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-09-13 13:48:04 -0300

    Início da construção da nova versão do Space Gallery.

--- src/componentes/Botao/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./Botao.module.scss";
-
-export default function Button({ children }) {
-  return <button className={styles.button}>{children}</button>;
-}
--- src/componentes/Menu/Icone/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Icone({ icone, styles }) {
-  return (
-    <li className={styles.menu__item}>
-      <img src={icone.path} alt={icone.alt} />
-      <a href="/">{icone.link}</a>
-    </li>
-  )
-}
