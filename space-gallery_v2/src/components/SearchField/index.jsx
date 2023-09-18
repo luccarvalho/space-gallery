2eeacaf8db5ac8566e38eacef5d56e196e550ab5
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-09-18 12:24:41 -0300

    Componentização de elementos do header.

---
+++ space-gallery_v2/src/components/SearchField/index.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import search from "./search.png";
+
+const ContainerEstilizado = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const SearchEstilizado = styled.input`
+  height: 56px;
+  padding: 12px 16px;
+  border-radius: 10px;
+  border: 2px solid;
+  border-color: #c98cf1;
+  background: transparent;
+  box-sizing: border-box;
+  width: 566px;
+  color: #d9d9d9;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+`;
+
+const SearchIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 38px;
+  height: 38px;
+`;
+
+const SearchField = () => {
+  return (
+    <ContainerEstilizado>
+      <SearchEstilizado placeholder="O que você procura?" />
+      <SearchIcon src={search} alt="ícone de lupa de pesquisa" />
+    </ContainerEstilizado>
+  );
+};
+
+export default SearchField;
