a02c234a849c5c936ac1d378e0443bd491f5044a
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-10-05 17:23:21 -0300

    Criando a lógica para filtrar imagens por tags.

--- space-gallery_v2/src/App.jsx
+++ space-gallery_v2/src/App.jsx
@@ -6,8 +6,9 @@ import Banner from "./components/Banner";
 import bannerBackground from "./assets/banner.png";
 import Gallery from "./components/Gallery";
 import fotos from "./fotos.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ModalZoom from "./components/ModalZoom";
+import Footer from "./components/Footer";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(
@@ -40,11 +41,44 @@ const GalleryContent = styled.section`
 const App = () => {
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
   const [fotoSelecionada, setFotoSelecionada] = useState(null);
+  const [filtro, setFiltro] = useState("");
+  const [tag, setTag] = useState(0);
+
+  useEffect(() => {
+    const fotosFiltradas = fotos.filter((foto) => {
+      const filtroPorTag = !tag || foto.tagId === tag;
+      const filtroPorTitulo =
+        !filtro || foto.titulo.toLowerCase().includes(filtro.toLowerCase());
+      return filtroPorTag && filtroPorTitulo;
+    });
+    setFotosDaGaleria(fotosFiltradas);
+  }, [filtro, tag]);
+
+  const aoAlternarFavorito = (foto) => {
+    if (foto.id === fotoSelecionada?.id) {
+      setFotoSelecionada({
+        ...fotoSelecionada,
+        favorita: !fotoSelecionada.favorita,
+      });
+    }
+    setFotosDaGaleria(
+      fotosDaGaleria.map((fotoDaGaleria) => {
+        return {
+          ...fotoDaGaleria,
+          favorita:
+            fotoDaGaleria.id === foto.id
+              ? !foto.favorita
+              : fotoDaGaleria.favorita,
+        };
+      })
+    );
+  };
+
   return (
     <FundoGradiente>
       <GlobalStyles />
       <AppContainer>
-        <Header />
+        <Header filtro={filtro} setFiltro={setFiltro} />
         <MainContainer>
           <Aside />
           <GalleryContent>
@@ -54,7 +88,9 @@ const App = () => {
             />
             <Gallery
               aoFotoSelecionada={(foto) => setFotoSelecionada(foto)}
+              aoAlternarFavorito={aoAlternarFavorito}
               fotos={fotosDaGaleria}
+              setTag={setTag}
             />
           </GalleryContent>
         </MainContainer>
@@ -62,7 +98,9 @@ const App = () => {
       <ModalZoom
         foto={fotoSelecionada}
         aoFechar={() => setFotoSelecionada(null)}
+        aoAlternarFavorito={aoAlternarFavorito}
       />
+      <Footer />
     </FundoGradiente>
   );
 };
